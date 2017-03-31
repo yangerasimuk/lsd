b52f767f96725b60417d3b2b3aadfaa0683162a5
Yan Gerasimuk <mail@yangerasimuk.com>
2017-03-31 11:32:11 +0300

    Create Xcode empty command line project

---
+++ lsd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lsd
+//
+//  Created by Ян on 31/03/2017.
+//  Copyright © 2017 Yan Gerasimuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
